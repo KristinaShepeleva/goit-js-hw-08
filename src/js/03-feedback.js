b505d12f942525e5afda6b78f9b6c0633c2bafff
KristinaShepeleva <shepelevakristina52@gmail.com>
2023-03-04 15:48:08 +0200

    3-fix

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -11,37 +11,104 @@ const refs = {
 };
 
 refs.form.addEventListener('submit', onFormSubmit);
-refs.form.addEventListener('input', throttle(onTextareaInput, 500));
+refs.form.addEventListener('input', throttle(onFormInput, 500));
 
 populateForm();
 
 function onFormSubmit(evt) {
   evt.preventDefault();
 
+  if (refs.textarea.value && refs.input.value) {
+
   evt.currentTarget.reset();
-  console.log(localStorage.getItem(STORAGE_KEY));
+  console.log(JSON.parse(localStorage.getItem(STORAGE_KEY)));
   localStorage.removeItem(STORAGE_KEY);
+  Object.getOwnPropertyNames(formData).forEach(key => (delete formData[key]));
+  } 
 }
 
 const formData = {};
 
-function onTextareaInput(e) {
-  //console.log(e.target.name);
-  formData[e.target.name] = e.target.value;
-  
+function onFormInput(e) {
+  formData[e.target.name] = e.target.value;    
   localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
-  //console.log(formData);
 }
 
 function populateForm() {
   const savedData = localStorage.getItem(STORAGE_KEY);
-  //console.log(savedData);
   const parsedData = JSON.parse(savedData);
-  //console.log(parsedData);
 
-  if (savedData) {
-    refs.textarea.value = parsedData.message;
-    refs.input.value = parsedData.email;
-
+  if (localStorage.getItem(STORAGE_KEY)) {
+    refs.input.value = parsedData.email || '';
+    refs.textarea.value = parsedData.message || '';
   }
 }
+
+
+
+
+
+
+
+
+//const STORAGE_KEY = 'feedback-msg';
+
+//const refs = {
+//  form: document.querySelector('.js-feedback-form'),
+//  textarea: document.querySelector('.js-feedback-form  textarea'),
+//};
+
+//refs.form.addEventListener('submit', onFormSubmit);
+//refs.textarea.addEventListener('input', throttle(onTextareaInput, 200));
+
+//populateTextarea();
+
+/*
+ * - Останавливаем поведение по умолчанию
+ * - Убираем сообщение из хранилища
+ * - Очищаем форму
+ */
+//function onFormSubmit(evt) {
+//  evt.preventDefault();
+
+ // console.log('Отправляем форму');
+ // evt.currentTarget.reset();
+//  localStorage.removeItem(STORAGE_KEY);
+//}
+
+/*
+ * - Получаем значение поля
+ * - Сохраняем его в хранилище
+ * - Можно добавить throttle
+ */
+//function onTextareaInput(evt) {
+ // const message = evt.target.value;
+
+//  localStorage.setItem(STORAGE_KEY, message);
+//}
+
+/*
+ * - Получаем значение из хранилища
+ * - Если там что-то было, обновляем DOM
+ */
+//function populateTextarea() {
+//  const savedMessage = localStorage.getItem(STORAGE_KEY);
+
+//  if (savedMessage) {
+ //   refs.textarea.value = savedMessage;
+//  }
+//}
+
+// Домой
+// сделать так чтобы сохраняло не только сообщение но и имя, и все в одном обьекте
+
+// const formData = {};
+
+// refs.form.addEventListener('input', e => {
+//   // console.log(e.target.name);
+//   // console.log(e.target.value);
+
+//   formData[e.target.name] = e.target.value;
+
+//   console.log(formData);
+// });
